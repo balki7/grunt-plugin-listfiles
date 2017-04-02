f8ffdbf3e24d59a023800aca3b118b3ebc6eddbd
Balkı Gemirter <balki1985@gmail.com>
2017-04-02 14:29:41 +0300

    Initial commit

---
+++ tasks/plugin_listfiles.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-plugin-listfiles
+ * https://github.com/balki7/grunt-plugin-listfiles
+ *
+ * Copyright (c) 2017 Balkı Gemirter
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('plugin_listfiles', 'The Grunt plugin that lists files in the given directory recursively.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
